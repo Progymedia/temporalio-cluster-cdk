c68db23531f236683150feab9a406a20baff83c5
James Watkins-Harvey <jwatkins@progi-media.com>
2022-03-24 11:40:18 -0400

    fix: do not crash if namespace creation failed

--- lib/customResources/temporal/TemporalNamespaceHandler.ts
+++ lib/customResources/temporal/TemporalNamespaceHandler.ts
@@ -72,13 +72,52 @@ function execTctlTool(context: IResolvedTemporalNamespaceResourceProps, command:
     args.push('--address', `${context.temporalHost}`);
     args.push('--auto_confirm');
 
-    execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
-        encoding: 'utf-8',
-        env,
-        stdio: [
-            'ignore', // ignore stdin
-            process.stderr, // redirect stdout to stderr
-            'inherit', // inherit stderr
-        ],
-    });
+    try {
+        execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
+            encoding: 'utf-8',
+            env,
+            stdio: [
+                'ignore', // ignore stdin
+                process.stderr, // redirect stdout to stderr
+                'inherit', // inherit stderr
+            ],
+        });
+    } catch (e) {
+        execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
+            encoding: 'utf-8',
+            env,
+            stdio: [
+                'ignore', // ignore stdin
+                process.stderr, // redirect stdout to stderr
+                'inherit', // inherit stderr
+            ],
+        });
+
+        // FIXME: Explicitly check for "Already exists" errors.
+        //
+        // Error: Register namespace operation failed.
+        // Error Details: rpc error: code = AlreadyExists desc = Namespace already exists.
+        // ERROR	Invoke Error 	{
+        //    "errorType": "Error",
+        //    "errorMessage": "Command failed: /opt/temporal/bin/tctl --address xxxx:7233 --auto_confirm --namespace xxxx namespace register",
+        //    "status": 1,
+        //    "signal": null,
+        //    "output": [
+        //        null,
+        //        null,
+        //        null
+        //    ],
+        //    "pid": 25,
+        //    "stdout": null,
+        //    "stderr": null,
+        //    "stack": [
+        //        "Error: Command failed: /opt/temporal/bin/tctl --address xxxx:7233 --auto_confirm --namespace xxxx namespace register",
+        //        "    at checkExecSyncError (child_process.js:790:11)",
+        //        "    at execFileSync (child_process.js:827:15)",
+        //        "    at execTctlTool (/var/task/index.js:2658:36)",
+        //        "    at createNamespace (/var/task/index.js:2651:3)",
+        //        "    at Runtime.onEvent [as handler] (/var/task/index.js:2618:7)"
+        //    ]
+        //}
+    }
 }
