ff7c9d88feba21a959d7e1c70c79a9f2f0e04345
James Watkins-Harvey <jwatkins@progi-media.com>
2022-08-26 14:29:10 -0400

    fix: Do not crash if namespace already exists

--- lib/customResources/temporal/TemporalNamespaceHandler.ts
+++ lib/customResources/temporal/TemporalNamespaceHandler.ts
@@ -1,5 +1,5 @@
 import { CdkCustomResourceEvent, CdkCustomResourceResponse } from 'aws-lambda';
-import { execFileSync } from 'child_process';
+import { execFileSync, SpawnSyncReturns } from 'child_process';
 import waitPort from 'wait-port';
 
 export interface ITemporalNamespaceResourceProperties {
@@ -62,7 +62,17 @@ function waitForTemporal(resourceProps: IResolvedTemporalNamespaceResourceProps)
 }
 
 function createNamespace(resourceProps: IResolvedTemporalNamespaceResourceProps) {
-    execTctlTool(resourceProps, ['--namespace', resourceProps.namespaceName, 'namespace', 'register']);
+    try {
+        execTctlTool(resourceProps, ['--namespace', resourceProps.namespaceName, 'namespace', 'register']);
+    } catch (e) {
+        if ((e as SpawnSyncReturns<string>).stderr.includes('AlreadyExists')) {
+            return;
+        }
+
+        console.error(`Failed to create namespace '${resourceProps.namespaceName}'`);
+        console.error(e);
+        process.exit(1);
+    }
 }
 
 function execTctlTool(context: IResolvedTemporalNamespaceResourceProps, command: string[]) {
@@ -74,52 +84,8 @@ function execTctlTool(context: IResolvedTemporalNamespaceResourceProps, command:
     args.push('--address', `${context.temporalHost}`);
     args.push('--auto_confirm');
 
-    try {
-        execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
-            encoding: 'utf-8',
-            env,
-            stdio: [
-                'ignore', // ignore stdin
-                process.stderr, // redirect stdout to stderr
-                'inherit', // inherit stderr
-            ],
-        });
-    } catch (e) {
-        execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
-            encoding: 'utf-8',
-            env,
-            stdio: [
-                'ignore', // ignore stdin
-                process.stderr, // redirect stdout to stderr
-                'inherit', // inherit stderr
-            ],
-        });
-
-        // FIXME: Explicitly check for "Already exists" errors.
-        //
-        // Error: Register namespace operation failed.
-        // Error Details: rpc error: code = AlreadyExists desc = Namespace already exists.
-        // ERROR	Invoke Error 	{
-        //    "errorType": "Error",
-        //    "errorMessage": "Command failed: /opt/temporal/bin/tctl --address xxxx:7233 --auto_confirm --namespace xxxx namespace register",
-        //    "status": 1,
-        //    "signal": null,
-        //    "output": [
-        //        null,
-        //        null,
-        //        null
-        //    ],
-        //    "pid": 25,
-        //    "stdout": null,
-        //    "stderr": null,
-        //    "stack": [
-        //        "Error: Command failed: /opt/temporal/bin/tctl --address xxxx:7233 --auto_confirm --namespace xxxx namespace register",
-        //        "    at checkExecSyncError (child_process.js:790:11)",
-        //        "    at execFileSync (child_process.js:827:15)",
-        //        "    at execTctlTool (/var/task/index.js:2658:36)",
-        //        "    at createNamespace (/var/task/index.js:2651:3)",
-        //        "    at Runtime.onEvent [as handler] (/var/task/index.js:2618:7)"
-        //    ]
-        //}
-    }
+    execFileSync('/opt/temporal/bin/tctl', [...args, ...command], {
+        encoding: 'utf-8',
+        env,
+    });
 }
